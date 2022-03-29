95bde72ee9051b8a4ada75df790a98c88ae43b7f
debanik <debanik@solarisrobots.in>
2022-03-29 23:47:27 +0530

    vector element access by pointer

--- basic_vector.cpp
+++ basic_vector.cpp
@@ -9,22 +9,32 @@ int main()
     int k=10;
     int sum=0;
 
-
     // vector index and element access
+    cout<<"vector index and element access"<<endl;
     for(int i=0; i<v.size();i++)
     {
         cout<<i<<' '<<v.at(i)<<endl;
     }
-    cout<<"-------------------------------";
+    cout<<"-------------------------------"<<endl;
 
     // vector element access from a loop
+    cout<<"vector element access from a loop"<<endl;
     for(int n:v)
     {
         cout<<n<<endl;
     }
-    cout<<"-------------------------------";
+    cout<<"-------------------------------"<<endl;
+
+    // vector element access by pointer
+    cout<<"vector element access by pointer"<<endl;
+    for(auto it=v.begin(); it !=v.end(); it++)
+    {
+        cout<<"---->"<<*it<<endl;
+    }
+    cout<<"-------------------------------"<<endl;
 
     // vector element sum and find a specific sum value with condition
+    cout<<"vector element sum and find a specific sum value with condition"<<endl;
     for(int i=0; i<v.size();i++)
     {
         for(int j=0; j<v.size();j++)
@@ -35,8 +45,7 @@ int main()
                 cout<<'['<<i<<','<<j<<']'<<"----->>"<<i<<' '<<j<<"--->"<<sum<<endl;
             }
         }
-
-        cout<<"-------------------------------";
     }
+    cout<<"-------------------------------"<<endl;
     return 0;
 }
