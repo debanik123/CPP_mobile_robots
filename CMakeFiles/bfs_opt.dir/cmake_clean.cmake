ae1e10b08cf6c8747d05d93725a89d74aa27f2c6
Debanik Roy <debanik@solarisrobots.in>
2022-09-15 23:09:24 +0530

    added Cmake_link

---
+++ CMakeFiles/bfs_opt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bfs_opt.dir/src/bfs_opt.cpp.o"
+  "bfs_opt"
+  "bfs_opt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bfs_opt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
