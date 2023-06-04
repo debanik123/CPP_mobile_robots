56cdbede1ef0c75a8aa32191d909049c05bd678b
Debanik <debanik>
2023-06-04 23:45:42 +0530

    added multiple thread fucntion

--- src/parallel_programming.cpp
+++ src/parallel_programming.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <thread>
+#include <string>
 
 void foo()
 {
-    printf("Hello from foo \n");
+    // printf("Hello from foo - \n", std::this_thread::get_id());
+    std::cout<<"Hello from foo "<< std::this_thread::get_id()<<std::endl;
+
 }
+
+class callable_class {
+    public:
+        void operator()()
+        {
+            // printf("Hello form callable class - \n", std::this_thread::get_id());
+            std::cout<<"Hello from callable_class "<< std::this_thread::get_id()<<std::endl;
+        }
+};
 
 int main(int argc, char **argv)
 {
     std::thread thread1(foo);
-    thread1.join();
 
-    printf("Hello from main \n"); 
+    callable_class obj1;
+    std::thread thread2(obj1);
+
+    std::thread thread3( [] {
+        // printf("Hello forom lambda function - \n", std::this_thread::get_id());
+    std::cout<<"Hello from lambda function "<< std::this_thread::get_id()<<std::endl;
+    });
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+
+
+    // printf("Hello from main - \n", std::this_thread::get_id()); 
+    std::cout<<"Hello from main "<< std::this_thread::get_id()<<std::endl;
     return 0;
 }
